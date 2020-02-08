7d0efb95a4d457121831101ad6e84a9978887143
YEPEJ010 <yepej010@apps.disney.com>
2020-02-08 11:35:17 -0500

    Lab1 first commit

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/labsaaaas_grxx/lab1/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.edu.udea.compumovil.labsaaaas_grxx.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
